784145a95446a939e0f7cd2cce7eb6af6942da61
pandurang patil <32433674+pandurang9921@users.noreply.github.com>
2018-08-21 14:02:16 +0530

    Create Command_line_arguments.sh

---
+++ Command_line_arguments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Script/Command name is => is $0"
+echo "arg1 => $1"
+echo "arg2 => $2"
+echo "arg3 => $3"
+echo "total no of arguments is - $#"
+echo "Script PID is - $$"
+echo "Status of previous command is - $?"
+name=$myname
+echo "Name - $name"
+read n
